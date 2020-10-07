32546969ebcea70fa58ff87955d95e8435302cf1
Andreas Kasprzok <akasprzok@gmail.com>
2020-10-07 12:05:07 -0700

    add retries to tesla client (#8)

--- lib/client/tesla_client.ex
+++ lib/client/tesla_client.ex
@@ -24,7 +24,16 @@ defmodule Logger.Backend.Humio.Client.Tesla do
     middleware = [
       {Tesla.Middleware.BaseUrl, base_url},
       {Tesla.Middleware.Headers, headers},
-      {Tesla.Middleware.Compression, format: "gzip"}
+      {Tesla.Middleware.Compression, format: "gzip"},
+      {Tesla.Middleware.Retry,
+       delay: 500,
+       max_retries: 10,
+       max_delay: 4_000,
+       should_retry: fn
+         {:ok, %{status: status}} when status in [400, 500] -> true
+         {:ok, _} -> false
+         {:error, _} -> true
+       end}
     ]
 
     Tesla.client(middleware)
